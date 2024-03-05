0719a140c363d1d25a8858c4d809b7f71e5d28aa
Jot786 <159805088+Jot786@users.noreply.github.com>
2024-03-05 14:56:08 +0000

    CRP-5 designed home page and retrieved data from db

--- src/main/java/com/aber/crp/web/HomePageController.java
+++ src/main/java/com/aber/crp/web/HomePageController.java
@@ -1,13 +1,18 @@
 package com.aber.crp.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.aber.crp.dto.PostDto;
 import com.aber.crp.dto.UserRegistrationDto;
+import com.aber.crp.service.PostService;
 import com.aber.crp.service.UserService;
 
 @Controller
@@ -15,8 +20,13 @@ import com.aber.crp.service.UserService;
 public class HomePageController {
 
 	
+	@Autowired
+	PostService postService;
+	
 	@GetMapping
-	public String showLoginPage() {
+	public String showLoginPage(Model model) {
+		List<PostDto> postDtoList = postService.findAllPost();
+		model.addAttribute("postList", postDtoList);
 		return "user/home";
 	}
 	
